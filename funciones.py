ddb6489e03dda4bb99e3005f2a1d626190897ec7
Juan Miguel Cadavid <juanmiscj25@gmail.com>
2021-09-14 02:57:58 -0500

    v2

--- funciones.py
+++ funciones.py
@@ -4,8 +4,13 @@ def cargarDatosEstudiantes(ruta):
     archivo.close()
     estudiantes = {}
     for i in data:
-        estudiantes[i.split(";")[0]] = i.split(";")[1][:-1]
+        estudiantes[int(i.split(";")[0])] = i.split(";")[1][:-1]
     return estudiantes
+
+def toFloat(lista):
+    for i in range(len(lista)):
+        lista[i] = float(lista[i])
+    return lista
 
 def cargarDatosCursos(ruta):
     archivo = open(ruta, "r")
@@ -15,10 +20,10 @@ def cargarDatosCursos(ruta):
     data = data.split("\n\n")[:-1]
     for i in data:
         curso = i.split("\n")
-        cursos[curso[0].split(";")[0]] = [curso[0].split(";")[1], curso[0].split(";")[2]]
+        cursos[curso[0].split(";")[0]] = [int(curso[0].split(";")[1]), int(curso[0].split(";")[2])]
         estudiantes = {}
         for j in curso[1:]:
-            estudiantes[j.split(";")[0]] = j.split(";")[1:]
+            estudiantes[int(j.split(";")[0])] = toFloat(j.split(";")[1:])
         cursos[curso[0].split(";")[0]].append(estudiantes)
     return cursos
 
@@ -36,7 +41,7 @@ def agregarEstudiante():
         try:
             cedula = int(cedula)
             break
-        except:
+        except ValueError:
             pass
 
     return cedula, nombre
@@ -54,23 +59,21 @@ def crearNuevoCurso():
         try:
             creditos = int(input("Número de créditos: "))
             break
-        except:
+        except ValueError:
             pass
     while True:
         try:
             notas = int(input("Número de notas: "))
             break
-        except:
+        except ValueError:
             pass
     return nombre, creditos, notas
 
 def actualizarCursos(ruta, datos):
-    print(datos)
     archivo = open(ruta, "w")
     txt = ""
     for i, j in datos.items():
         txt += f"{i};{j[0]};{j[1]}\n"
-        print(j[2])
         for k, l in j[2].items():
             txt += f"{k};"
             for n in l:
@@ -79,3 +82,51 @@ def actualizarCursos(ruta, datos):
         txt += "\n"
     archivo.write(txt)
     archivo.close()
+
+def cedulaEstudiante(estudiantes):
+    while True:
+        try:
+            cedula = int(input("Ingrese el número de identificación del estudiante: "))
+            estudiantes[cedula]
+            return cedula
+        except (ValueError, KeyError):
+            print("Este estudiante no está registrado")
+
+def nombreCurso(cursos):
+    while True:
+        curso = input("Ingrese el nombre del curso: ")
+        try:
+            cursos[curso]
+            return curso
+        except:
+            print("Este curso no existe")
+
+def printAdvice(string):
+    print()
+    print("*"*(len(string)+4)+f"\n* {string} *\n"+"*"*(len(string)+4))
+    print()
+
+def mostrarCursos(cursos):
+    print("\nCURSOS")
+    for i, j in cursos.items():
+        print(f"\nNombre del curso: {i}\nNúmero de créditos: {j[0]}\nNúmero de notas: {j[1]}\nNúmero de estudiantes inscritos: {len(j[2])}\n")
+
+def mostrarEstudiantes(estudiantes):
+    print("\nESTUDIANTES")
+    for i, j in estudiantes.items():
+        print(f"\nNombre del estudiante: {j}\nNúmero de identificación: {i}\n")
+
+def calcularPromedioPonderado(estudiante, cursos):
+    sumatoria = 0
+    creditos = 0
+    for i, j in cursos.items():
+        try:
+            notaFinal = 0
+            for nota in j[2][estudiante]:
+                notaFinal += nota
+            notaFinal = notaFinal/j[0]
+            sumatoria += notaFinal*j[1]
+            creditos += j[1]
+        except KeyError:
+            pass
+    return sumatoria/creditos
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from funciones import *
 
-estudiantes = cargarDatosEstudiantes("./students.txt")
-cursos = cargarDatosCursos("./subjects.txt")
+subjectsRoot = "./subjects.txt"
+studentsRoot = "./students.txt"
+
+estudiantes = cargarDatosEstudiantes(studentsRoot)
+cursos = cargarDatosCursos(subjectsRoot)
+
+print(cursos)
+print(estudiantes)
 
 while True:
     menu = """
@@ -10,16 +16,77 @@ while True:
 3. Matricular estudiante en un curso
 4. Mostrar todos los cursos registrados
 5. Mostrar todos los estudiantes registrados
+6. Consultar promedio de estudiante
+7. Consultar materias matriculadas por estudiante
+8. Consultar estudiantes matriculados en un curso
+9. Porcentaje de estudiantes que ganó y perdió un curso
+10. Salir
 """
 
     print(menu)
 
-    op = int(input("Seleccione una opción: "))
+    try:
+        op = int(input("Seleccione una opción: "))
+    except ValueError:
+        op = None
     if op == 1:
         estudiante = agregarEstudiante()
         estudiantes[estudiante[0]] = estudiante[1]
-        actualizarEstudiantes("./students.txt", estudiantes)
+        actualizarEstudiantes(studentsRoot, estudiantes)
     elif op == 2:
         curso = crearNuevoCurso()
         cursos[curso[0]] = [curso[1], curso[2], {}]
-        actualizarCursos("./subjects2.txt", cursos)
+        actualizarCursos(subjectsRoot, cursos)
+    elif op == 3:
+        estudiante = cedulaEstudiante(estudiantes)
+        curso = nombreCurso(cursos)
+        try:
+            cursos[curso][2][estudiante]
+            printAdvice("Este estudiante ya está registrado en este curso")
+        except:
+            cursos[curso][2][estudiante] = []
+            actualizarCursos(subjectsRoot, cursos)
+    elif op == 4:
+        mostrarCursos(cursos)
+    elif op == 5:
+        mostrarEstudiantes(estudiantes)
+    elif op == 6:
+        estudiante = cedulaEstudiante(estudiantes)
+        promedio = calcularPromedioPonderado(estudiante, cursos)
+        print(f"El promedio de este estudiante es: {round(promedio, 2)}")
+    elif op == 7:
+        estudiante = cedulaEstudiante(estudiantes)
+        print("\nCursos en los que el estuidante está matriculado:\n")
+        none = True
+        for i, j in cursos.items():
+            try:
+                j[2][estudiante]
+                print(i)
+                none = False
+            except KeyError:
+                pass
+        if none:
+            print("Este estudiante no está matriculado en ningún curso")
+    elif op == 8:
+        curso = nombreCurso(cursos)
+        if len(cursos[curso][2]) > 0:
+            print(f"\nEstudiantes matriculados en {curso}:\n")
+            for i in cursos[curso][2].keys():
+                print(f"\nNombre del estudiante: {estudiantes[i]}\nNúmero de identificación del estudiante: {i}\n")
+        else:
+            print("\nNo existen estudiantes matriculados a este curso")
+    elif op == 9:
+        curso = nombreCurso(cursos)
+        passed = 0
+        for i in cursos[curso][2].values():
+            nota = 0
+            for j in i:
+                nota += j
+            nota = nota/cursos[curso][0]
+            if nota >= 3:
+                passed += 1
+        print(f"\nEstudiantes que ganaron el curso: {(passed*100)/len(cursos[curso][2])}%\nEstudiantes que perdieron el curso: {100-((passed*100)/len(cursos[curso][2]))}%")
+    elif op == 10:
+        break
+    else:
+        print("Debes seleccionar una opción válida")
